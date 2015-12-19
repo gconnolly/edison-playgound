ce681e2a980410d3bb9fbbde4ad4d3be647bc8f8
Gene Connolly <connolly.gene@gmail.com>
2015-12-18 19:58:14 -0800

    Update five.js

--- five.js
+++ five.js
@@ -3,9 +3,8 @@ var Edison = require("edison-io");
 var board = new five.Board({
   io: new Edison()
 });
-console.log('test');
+
 board.on("ready", function() {
-  console.log('running');
   var led = new five.Led(13);
   led.blink(500);
 });
