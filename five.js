2d07e61d4d201b5491dd409095b1f9b4b5e8ff02
geneconnolly <gene.connolly@meltwater.com>
2015-12-19 09:54:28 -0800

    Use static html page

--- five.js
+++ five.js
@@ -1,18 +1,12 @@
 var five = require("johnny-five");
 var Edison = require("edison-io");
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write('<!DOCTYPE "html">');
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page!!!</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
+  var fileStream = fs.createReadStream('index.html');
+  fileStream.pipe(response);
   response.end();
 });
 
