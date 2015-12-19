e6941fbae0bd28d855ca7cbdd473acd7a9d8fdca
Gene Connolly <connolly.gene@gmail.com>
2015-12-19 09:34:37 -0800

    Add web server

--- five.js
+++ five.js
@@ -1,5 +1,23 @@
 var five = require("johnny-five");
 var Edison = require("edison-io");
+var http = require("http");
+
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page!!!</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
+
+server.listen(80);
+
 var board = new five.Board({
   io: new Edison()
 });
