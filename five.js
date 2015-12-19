4c63100cf49e9f9676ceb51502864ff676716886
Gene Connolly <connolly.gene@gmail.com>
2015-12-18 20:32:09 -0800

    sos

--- five.js
+++ five.js
@@ -5,6 +5,27 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-  var led = new five.Led(13);
-  led.blink(500);
+  var led = new five.Led(13),
+      short = blink.bind(null, 1000),
+      long = blink.bind(null, 2000);
+  
+  short(
+    short.bind(null, 
+      short.bind(null, 
+        long.bind(null,
+          long.bind(null,
+            long.bind(null,
+              short.bind(null,
+                short.bind(null,
+                  short.bind(null,)))))))));
+  
 });
+
+function blink(interval, callback) {
+  var led = new five.Led(13);
+  led.on();
+  setTimeout(function () {
+    led.off();
+    callback();
+  }, interval);
+}
