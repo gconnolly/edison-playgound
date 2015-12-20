efb2649cc5133b305b47e446e315e9e4324539e3
geneconnolly <gene.connolly@meltwater.com>
2015-12-20 08:07:26 -0800

    servo pin change

--- five.js
+++ five.js
@@ -22,7 +22,7 @@ board.on("ready", function() {
       long = blink.bind(null, led, 1000);
       
       
-  var servo = new five.Servo.Continuous(12);
+  var servo = new five.Servo.Continuous(13);
   
   servo.sweep();
 
