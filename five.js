594bbacf2b368b589c04c39bf7d6963b0ad1ee4e
geneconnolly <gene.connolly@meltwater.com>
2015-12-24 15:40:06 -0800

    temporarily remove arduino components

--- five.js
+++ five.js
@@ -1,5 +1,5 @@
-var five = require("johnny-five"),
-    Edison = require("edison-io"),
+var //five = require("johnny-five"),
+    //Edison = require("edison-io"),
     express = require("express"),
     fs = require("fs"),
     app = express(),
@@ -7,45 +7,43 @@ var five = require("johnny-five"),
 
 app.use(express.static('public'));
 
-var board = new five.Board({
-  io: new Edison()
-});
+//var board = new five.Board({
+//  io: new Edison()
+//});
 
 board.on("ready", function() {
-  var rightServo = new five.Servo.Continuous(5),
-      leftServo = new five.Servo.Continuous(6);
-
-  //led.on();
-
-  // Add to REPL
-  this.repl.inject({
-    //led: led,
-    rightServo: rightServo,
-    leftServo: leftServo
-  });
+//  var rightServo = new five.Servo.Continuous(5),
+//      leftServo = new five.Servo.Continuous(6);
+  
+//   // Add to REPL
+//   this.repl.inject({
+//     //led: led,
+//     rightServo: rightServo,
+//     leftServo: leftServo
+//   });
 
   app.post('/forward', function(sReq, sRes){
-    rightServo.cw(1);
-    leftServo.ccw(1);
+//    rightServo.cw(1);
+//    leftServo.ccw(1);
   });
   
   app.post('/backward', function(sReq, sRes){
-    rightServo.ccw(1);
-    leftServo.cw(1);
+//    rightServo.ccw(1);
+//    leftServo.cw(1);
   });
 
   app.post('/right', function(sReq, sRes){
-    rightServo.ccw(1);
-    leftServo.ccw(1);
+//    rightServo.ccw(1);
+//    leftServo.ccw(1);
   });
 
   app.post('/left', function(sReq, sRes){
-    rightServo.cw(1);
-    leftServo.cw(1);
+//    rightServo.cw(1);
+//    leftServo.cw(1);
   });
   
   app.post('/stop', function(sReq, sRes){
-    rightServo.cw(0);
-    leftServo.cw(0);
+//    rightServo.cw(0);
+//    leftServo.cw(0);
   });
 });
