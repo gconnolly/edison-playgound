d022648168ae4a98f3620781b5430d78d917721c
geneconnolly <gene.connolly@meltwater.com>
2015-12-24 17:17:05 -0800

    test led

--- five.js
+++ five.js
@@ -12,38 +12,42 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-  var rightServo = new five.Servo.Continuous(5),
-      leftServo = new five.Servo.Continuous(6);
+//  var rightServo = new five.Servo.Continuous(5),
+//      leftServo = new five.Servo.Continuous(6);
   
+  
+    var led = new five.Led(13);
 //   // Add to REPL
 //   this.repl.inject({
 //     //led: led,
 //     rightServo: rightServo,
 //     leftServo: leftServo
 //   });
-
+    
   app.post('/forward', function(sReq, sRes){
-    rightServo.cw(1);
-    leftServo.ccw(1);
+//    rightServo.cw(1);
+//    leftServo.ccw(1);
+    led.on();
   });
   
   app.post('/backward', function(sReq, sRes){
-    rightServo.ccw(1);
-    leftServo.cw(1);
+//    rightServo.ccw(1);
+//    leftServo.cw(1);
   });
 
   app.post('/right', function(sReq, sRes){
-    rightServo.ccw(1);
-    leftServo.ccw(1);
+//    rightServo.ccw(1);
+//    leftServo.ccw(1);
   });
 
   app.post('/left', function(sReq, sRes){
-    rightServo.cw(1);
-    leftServo.cw(1);
+//    rightServo.cw(1);
+//    leftServo.cw(1);
   });
   
   app.post('/stop', function(sReq, sRes){
-    rightServo.cw(0);
-    leftServo.cw(0);
+//    rightServo.cw(0);
+//    leftServo.cw(0);
+    led.off();
   });
 });
