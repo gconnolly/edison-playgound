8b089739f87fa97cb23695316a14e559187e7ac9
geneconnolly <gene.connolly@meltwater.com>
2015-12-28 21:30:58 -0800

    styling the remote control; organize code;

--- five.js
+++ five.js
@@ -1,30 +1,21 @@
 var five = require("johnny-five"),
     Edison = require("edison-io"),
+    board = new five.Board({
+        io: new Edison()
+    }),
     express = require("express"),
-    fs = require("fs"),
     app = express(),
     server = app.listen(3000);
 
 app.use(express.static('public'));
-
-var board = new five.Board({
-  io: new Edison()
-});
 
 board.on("ready", function() {
   var rightServo = new five.Servo.Continuous(5),
       leftServo = new five.Servo.Continuous(6),
       led = new five.Led(13),
       piezo = new five.Piezo(12),
-      moving = false,
-      stop = function () {
-        moving = false;
-        rightServo.cw(0);
-        leftServo.cw(0);
-      };
+      la_cucaracha = '- - C C - C C - C C - F F F F - A A A A - -';
 
-  led.on();
-  
   // Add to REPL
   this.repl.inject({
     led: led,
@@ -34,12 +25,7 @@ board.on("ready", function() {
   });
 
   app.post('/forward', function(sReq, sRes){
-    if(moving) {
-        stop();
-        return;
-    } else {
-        moving = true;
-    }
+    led.blink();
     
     rightServo.cw(1);
     leftServo.ccw(1);
@@ -48,12 +34,7 @@ board.on("ready", function() {
   });
   
   app.post('/backward', function(sReq, sRes){
-    if(moving) {
-        stop();
-        return;
-    } else {
-        moving = true;
-    }
+    led.blink();
     
     rightServo.ccw(1);
     leftServo.cw(1);
@@ -62,12 +43,7 @@ board.on("ready", function() {
   });
 
   app.post('/right', function(sReq, sRes){
-    if(moving) {
-        stop();
-        return;
-    } else {
-        moving = true;
-    }
+    led.blink();
     
     rightServo.ccw(0.5);
     leftServo.ccw(0.5);
@@ -76,13 +52,8 @@ board.on("ready", function() {
   });
 
   app.post('/left', function(sReq, sRes){
-    if(moving) {
-        stop();
-        return;
-    } else {
-        moving = true;
-    }
-    
+    led.blink();
+      
     rightServo.cw(0.5);
     leftServo.cw(0.5);
     
@@ -90,6 +61,8 @@ board.on("ready", function() {
   });
   
   app.post('/stop', function(sReq, sRes){
+    led.blink();
+      
     rightServo.cw(0);
     leftServo.cw(0);
     
@@ -97,11 +70,13 @@ board.on("ready", function() {
   });
   
   app.post('/honk', function(sReq, sRes){
+    led.blink();
+    
     piezo.play({
         // song is composed by a string of notes
         // a default beat is set, and the default octave is used
         // any invalid note is read as "no note"
-        song: "- - C C C - C C C - C C C - F F F F - A A A A - - - - C C C - C C C - C C C - F F F F - A A A A - -",
+        song: la_cucaracha + la_cucaracha,
         beats: 1 / 8,
         tempo: 100
     });
