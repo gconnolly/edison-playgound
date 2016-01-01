ab3eafdee29738c532259143b000c619f2b644d7
geneconnolly <gene.connolly@meltwater.com>
2016-01-01 13:48:29 -0800

    wrap modules in IIFE's

--- rover-logger.js
+++ rover-logger.js
@@ -1,29 +1,31 @@
-var la_cucaracha = '- - C C - C C - C C - F F F F - A A A A - -',
-    RoverLogger = function RoverLogger(board) {
+(function () {
+    var la_cucaracha = '- - C C - C C - C C - F F F F - A A A A - -',
+        RoverLogger = function RoverLogger() {
+        };
+
+    RoverLogger.prototype.forward = function forward() {
+        console.log('forward');
     };
 
-RoverLogger.prototype.forward = function forward() {
-    console.log('forward');
-}
+    RoverLogger.prototype.backward = function backward() {
+        console.log('backward');
+    };
 
-RoverLogger.prototype.backward = function backward() {
-    console.log('backward');
-}
+    RoverLogger.prototype.right = function right() {
+        console.log('right');
+    };
 
-RoverLogger.prototype.right = function right() {
-    console.log('right');
-}
+    RoverLogger.prototype.left = function left() {
+        console.log('left');
+    };
 
-RoverLogger.prototype.left = function left() {
-    console.log('left');
-}
+    RoverLogger.prototype.stop = function stop() {
+        console.log('stop');
+    };
 
-RoverLogger.prototype.stop = function stop() {
-    console.log('stop');
-}
-
-RoverLogger.prototype.honk = function honk() {
-    console.log('honk: ' + la_cucaracha);
-}
-  
-module.export = RoverLogger;
+    RoverLogger.prototype.honk = function honk() {
+        console.log('honk: ' + la_cucaracha);
+    };
+    
+    module.export = RoverLogger;
+})();
