f0050c34cb0c875b2b99fc6c6ee85a1d9402be27
geneconnolly <gene.connolly@meltwater.com>
2016-01-01 13:17:56 -0800

    back to basics, stripping out most

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ var five = require("johnny-five"),
     board = new five.Board({
         io: new Edison()
     }),
-    express = require("express"),
-    webServer = express(),
-    Rover = require("./rover"),
-    RoverLogger = require("./rover-logger"),
-    RemoteControlWebServer = require("./remote-control-web-server"),
-    rover,
+    //express = require("express"),
+    //webServer = express(),
+    //Rover = require("./rover"),
+    //RoverLogger = require("./rover-logger"),
+    //RemoteControlWebServer = require("./remote-control-web-server"),
+    //rover,
     remoteControl;
 
 
-webServer.listen(3000);
+//webServer.listen(3000);
 console.log(board);
 board.on("ready", function onReady() {
-    rover = new RoverLogger(board);
-    remoteControl = new RemoteControlWebServer(webServer, rover);
+    //rover = new RoverLogger(board);
+    //remoteControl = new RemoteControlWebServer(webServer, rover);
+    console.log('test');
 });
